8fe66ebeeb60b6291cb9f289b30808025b1a0009
malik shakaa <malik.a.shakaa@gmail.com>
2022-06-07 12:50:04 +0300

    first react

---
+++ mern/react/intro to react/my_first_project/src/components/Body.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+class Body extends Component{
+    render(){
+        return(
+            <>
+            <h2>Things I need to do:</h2>
+            <ul>
+                <li>Learn React</li>
+                <li>Climb Everest</li>
+                <li>Run</li>
+                <li>Feed the dogs</li>
+  
+            </ul>
+            </>
+        );
+    };
+}
+export default Body;
---
+++ mern/react/intro to react/my_first_project/src/components/Header.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+class Header extends Component{
+    render(){
+        return(
+            <h1>Hello Dojo</h1>
+        );
+    };
+}
+export default Header;
