95509e70a8a2a583f2d2b4195eb8bf5c7aa45777
malik shakaa <malik.a.shakaa@gmail.com>
2022-06-16 12:13:32 +0300

    upload3

--- mern/Express/Faker_API/server.js
+++ mern/Express/Faker_API/server.js
@@ -1,46 +1,62 @@
 
 const express = require("express");
-const faker = require("faker");
+
+const  faker  = require("faker");
 const app = express();
 const port = 8000;
+class Company{
+    constructor(id,name, address){
+        this.id=id;
+        this.name=name;
+        this.address=address;
 
+    }
+}
 
 app.use( express.json() );
 app.use( express.urlencoded({ extended: true }) );
 
 
-const createUser = () => {
-    const newFake = {
-        password: faker.internet.password(),
-        email: faker.internet.email(), 
-        phoneNumber:faker.phone.phoneNumber(),
-        lastName:faker.name.lastName(),
-        firstName:faker.name.firstName(),
-        userId: faker.datatype.uuid(),
-    };
-    return newFake;
-};
-
-
-const creatCompany = ()=>{
-    const newFake = {
-        id: faker.datatype.uuid(),
-        name: faker.company.companyName(), 
-        street:faker.address.street(),
-        city:faker.address.cityName(),
-        state:faker.address.state(),
-        zipCode: faker.address.zipCode(),
-        country:faker.address.county(),
-    };
-    return newFake;
-
+class User{
+    constructor(id,firstName, lastName , phoneNumber,password,email){
+        this.id=faker.datatype.uuid();
+        this.firstName=faker.name.firstName();
+        this.lastName=faker.name.lastName();
+        this.phoneNumber= faker.phone.phoneNumber();
+        this.password=faker.internet.password();
+        this.email= faker.internet.email();
+    }
 }
 
+
+const createCompany = () => {
+    const newCompany = new Company(
+        faker.datatype.uuid(),
+        faker.company.companyName(),
+        {
+            street: faker.address.streetName(),
+            city: faker.address.cityName(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCode(),
+            country: faker.address.country(),
+        }
+    );
+    return newCompany;
+};
+
 app.get("/api/users/new", (req, res) => {
-    const newuser = createUser();
-    res.json( newuser );
+    const newFake=new User();
+    res.json( newFake );
 });
+
 app.get("/api/companies/new", (req, res) => {
-    const newcompany = creatCompany();
-    res.json( newcompany );
+    const newCompany=createCompany();
+    res.json( newCompany );
 });
+app.get("/api/user/company", (req, res) => {
+    const newCompany=createCompany();
+    const newFake=new User();
+    const arr=[newCompany,newFake]
+    res.json( arr  );
+});
+app.listen( port, () => console.log(`Listening on port: ${port}`) );
